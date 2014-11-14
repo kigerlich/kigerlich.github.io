5bbf9eddfc24a7d069f7fdbb961adb7d43576ff0
Kaleigh Gerlich <gerlich.kaleigh@gmail.com>
2014-11-14 13:50:40 -0800

    Adding active classes

--- indexjs.js
+++ indexjs.js
@@ -1,20 +1,20 @@
 $(document).ready(function() {
-	$('.Home').show().addClass('.active');
-	$('.Projects').hide().removeClass('.active');
-	$('.Contact').hide().removeClass('.active');
+	$('.Home').show().addClass('active');
+	$('.Projects').hide().removeClass('active');
+	$('.Contact').hide().removeClass('active');
 	$('.homelink').click(function() {
-		$('.Home').show().addClass('.active');
-		$('.Projects').hide().removeClass('.active');
-		$('.Contact').hide().removeClass('.active');
+		$('.Home').show().addClass('active');
+		$('.Projects').hide().removeClass('active');
+		$('.Contact').hide().removeClass('active');
 	})
 	$('.projectlink').click(function() {
-		$('.Home').hide().removeClass('.active');
-		$('.Projects').show().addClass('.active');
-		$('.Contact').hide().removeClass('.active');
+		$('.Home').hide().removeClass('active');
+		$('.Projects').show().addClass('active');
+		$('.Contact').hide().removeClass('active');
 	})
 	$('.contactlink').click(function() {
-		$('.Home').hide().removeClass('.active');
-		$('.Projects').hide().removeClass('.active');
-		$('.Contact').show().addClass('.active');
+		$('.Home').hide().removeClass('active');
+		$('.Projects').hide().removeClass('active');
+		$('.Contact').show().addClass('active');
 	})
 });
