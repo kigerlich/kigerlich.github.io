d7ea10a6ebd667f3f317bce86de43f68a77c1e58
Kaleigh Gerlich <gerlich.kaleigh@gmail.com>
2014-11-14 13:14:34 -0800

    Updating .show/.hide

--- indexjs.js
+++ indexjs.js
@@ -1,6 +1,18 @@
 $(document).ready(function() {
 	$('body').scrollspy({ target: '.nav masthead-nav' })
-	$('.nav masthead-nav li').click(function() {
-		console.log("click");	
+	$('.homelink').click(function() {
+		$('.inner cover').show();
+		$('.Projects').hide();
+		$('.Contact').hide();
+	})
+	$('.projectlink').click(function() {
+		$('.inner cover').hide();
+		$('.Projects').show();
+		$('.Contact').hide();
+	})
+	$('.contactlink').click(function() {
+		$('.inner cover').hide();
+		$('.Projects').hide();
+		$('.Contact').show();
 	})
 });
