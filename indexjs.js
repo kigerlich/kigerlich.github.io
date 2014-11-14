4154e83d205a92209299e8b3cff2cff0c618e359
Kaleigh Gerlich <gerlich.kaleigh@gmail.com>
2014-11-14 13:06:16 -0800

    Setting up click events

--- indexjs.js
+++ indexjs.js
@@ -1,3 +1,6 @@
 $(document).ready(function() {
-	$('body').scrollspy({ target: ".nav masthead-nav" })
+	$('body').scrollspy({ target: '.nav masthead-nav' })
+	$('.nav masthead-nav li').click(function() {
+		console.log("click");	
+	})
 });
